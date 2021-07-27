114a8b8905c660540f98269109844c6522eaaa02
lgs <1961621226@qq.com>
2021-07-27 09:04:55 +0800

    增加添加按钮

--- app/src/main/java/com/kamimi/lcalendar/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/kamimi/lcalendar/ui/notifications/NotificationsFragment.java
@@ -2,13 +2,19 @@ package com.kamimi.lcalendar.ui.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.kamimi.lcalendar.R;
 import com.kamimi.lcalendar.databinding.FragmentNotificationsBinding;
+
+import org.jetbrains.annotations.NotNull;
 
 public class NotificationsFragment extends Fragment {
 
@@ -19,6 +25,8 @@ public class NotificationsFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
 
+        // 显示右上角按钮
+        setHasOptionsMenu(true);
         // 创建堆叠滑动列表
         NotificationListAdapter adapter = new NotificationListAdapter(getContext());
         binding.pileLayout.setAdapter(adapter);
@@ -26,6 +34,22 @@ public class NotificationsFragment extends Fragment {
         adapter.reloadDataList();
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NotNull Menu menu, @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.notification_add_button, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.notification_add_button) {
+            // TODO
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
